c40f22817db24d9446af8b46ba1ae88a5ff0958b
dnsard077 <92017136+dnsard077@users.noreply.github.com>
2022-02-25 14:31:46 +0700

    Update index.js

--- pc1/index.js
+++ pc1/index.js
@@ -1,1 +1,21 @@
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
 
+socket.on('listening', function () {
+    socket.setBroadcast(true)
+	const address = socket.address();
+	console.log('UDP socket listening on ' + address.address + ":" + address.port);
+});
+
+socket.on('message', function (message, remote) {
+	console.log('SERVER RECEIVED:', remote.address + ':' + remote.port +' - ' + message);
+	const raw_data = {
+        random: Math.ceil(Math.random() * 100),
+	    random2: Math.ceil(Math.random() * 100)
+    };
+    const data = JSON.stringify(raw_data);    
+	socket.send(data, 0, data.length, remote.port, remote.address);
+
+});
+
+socket.bind('5555');
